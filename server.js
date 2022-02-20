6fb42a4e00ad4c7ba1841cff546d3e3368db606d
riohermawan10 <riohermawan999@gmail.com>
2022-02-20 16:43:51 +0700

    fixing 7

--- server.js
+++ server.js
@@ -13,29 +13,29 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//config upload
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, Date.now() + '-' + name);
-    }
-});
+// //config upload
+// const fileStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'images');
+//     },
+//     filename: (req, file, cb) => {
+//         const name = file.originalname.toLowerCase().split(' ').join('-');
+//         cb(null, Date.now() + '-' + name);
+//     }
+// });
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
+// const fileFilter = (req, file, cb) => {
+//     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+//         cb(null, true);
+//     } else {
+//         cb(null, false);
+//     }
+// }
 
-app.use(multer({
-    storage: fileStorage,
-    fileFilter: fileFilter
-}).single('image'));
+// app.use(multer({
+//     storage: fileStorage,
+//     fileFilter: fileFilter
+// }).single('image'));
 
 //load routing
 const UserRouting = require('./routes/user');
