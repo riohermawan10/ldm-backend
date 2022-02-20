5a9c29ea0024c1f1197c9202a78e47a420b367e8
riohermawan10 <riohermawan999@gmail.com>
2022-02-20 16:48:38 +0700

    fixing 8

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,7 @@
 const Sequelize = require('sequelize');
-// const config = require('../utils/config');
-// const config = process.env;
 const connection = new Sequelize("sql6474105", "sql6474105", "AsAaXurmFE", {
     host: "sql6.freemysqlhosting.net",
     dialect: "mysql",
-    // dialect: process.env.dialect,
     define: {
         timestamps: false
     }
