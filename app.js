f6e77a296a383f77b03e2e97caaddddf217878bb
riohermawan10 <riohermawan999@gmail.com>
2022-02-20 17:15:51 +0700

    fixing 13

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, async () => {
     try {
         await connection.authenticate();
-        await connection.sync({ force: false });
+        // await connection.sync({ force: false });
         console.log('Connection has been connect in database and server port: ' + port);
     } catch (error) {
         console.error('Unable to connect to the database: ', error.message);
